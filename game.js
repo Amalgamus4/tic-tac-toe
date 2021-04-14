a24dcd8d8aca820e47640dd6785770f09897b0c1
Ryan Ward <rc.ward@yahoo.com>
2021-04-13 20:50:36 -0600

    Refactor code

--- game.js
+++ game.js
@@ -1,14 +1,7 @@
 var turn = 'X';
-
-document.getElementById("box1").addEventListener("click", makeMove);
-document.getElementById("box2").addEventListener("click", makeMove);
-document.getElementById("box3").addEventListener("click", makeMove);
-document.getElementById("box4").addEventListener("click", makeMove);
-document.getElementById("box5").addEventListener("click", makeMove);
-document.getElementById("box6").addEventListener("click", makeMove);
-document.getElementById("box7").addEventListener("click", makeMove);
-document.getElementById("box8").addEventListener("click", makeMove);
-document.getElementById("box9").addEventListener("click", makeMove);
+for (i = 1; i < 9; i++){
+document.getElementById("box" + i).addEventListener("click", makeMove);
+}
 
 function makeMove(){
  if (document.getElementById(this.id).innerHTML == ' ') {
@@ -47,15 +40,9 @@ function declareWinner() {
         document.getElementById('turn').innerHTML = turn + " wins!";
         removeListeners();
       }
-      
+
 function removeListeners() {
-        document.getElementById("box1").removeEventListener("click", makeMove);
-        document.getElementById("box2").removeEventListener("click", makeMove);
-        document.getElementById("box3").removeEventListener("click", makeMove);
-        document.getElementById("box4").removeEventListener("click", makeMove);
-        document.getElementById("box5").removeEventListener("click", makeMove);
-        document.getElementById("box6").removeEventListener("click", makeMove);
-        document.getElementById("box7").removeEventListener("click", makeMove);
-        document.getElementById("box8").removeEventListener("click", makeMove);
-        document.getElementById("box9").removeEventListener("click", makeMove);
+  for (i = 1; i < 9; i++){
+        document.getElementById("box" + i).removeEventListener("click", makeMove);
+        }
       }
